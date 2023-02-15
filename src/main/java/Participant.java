48d350ab7b5c5bbdbb5044f07a7304c5b685b291
Gareth Shippen <garethshippen@yahoo.co.uk>
2023-02-15 19:19:59 +0000

    Cleaned up magic numbers.

--- src/main/java/Participant.java
+++ src/main/java/Participant.java
@@ -1,9 +1,45 @@
-public class Participent implements Runnable
+public class Participant implements Runnable
 {
-    
+    private int dorsal = 0;
+    private int raceLength = 0; //metres
+    private int remainingLength = 0; //meters
+    private Referee theReferee = null;
+
+    public Participant(Referee _raceReferee, int _newDorsal, int _newRaceLength)
+    {
+        theReferee = _raceReferee;
+        raceLength = _newRaceLength;
+        dorsal = _newDorsal + 1;
+    }
+
+    public int getDorsal()
+    {
+        return dorsal;
+    }
+
     @Override
     public void run()
     {
+        //1. Announce that you are starting the race
+        System.out.printf("Participant %d: Starting race\n\n", dorsal);
 
+        /*
+        2. Run the race, loop decrementing the remaining
+        length 1 metre at a time, and announce the crossing
+        of each km mark.
+        */
+
+        int kmMark = 0;
+        for(remainingLength = raceLength; remainingLength > 0; remainingLength--)
+        {
+            if(remainingLength % 1000 == 0)
+            {
+                System.out.printf("Participant %d: Passed km %d.\n\n", dorsal, ++kmMark);
+            }
+        }
+
+        //3. Announce that you have finished the race.
+        System.out.printf("Participant %d: Finished race.\n\n", dorsal);
+        theReferee.finished(dorsal);
     }
 }
--- src/main/java/Referee.java
+++ src/main/java/Referee.java
@@ -1,2 +1,108 @@
-package PACKAGE_NAME;public class Referee {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+public class Referee extends Thread
+{
+    private int raceLength = 0;
+    private int nParticipants = 0;
+    private List<Participant> participants = null;
+    private List<Thread> participantThreads = null;
+    private CountDownLatch finishLineTape = null;
+    private String winner = null;
+
+    public Referee(int _newRaceLength)
+    {
+        raceLength = _newRaceLength;
+        finishLineTape = new CountDownLatch(1);
+        participants = new ArrayList<>();
+        participantThreads = new ArrayList<>();
+    }
+
+    public void setNumberOfParticipants(int _nParticipants)
+    {
+        this.nParticipants = _nParticipants;
+    }
+
+    public void registerParticipant(int _newDorsal)
+    {
+        Participant participant = new Participant(this, _newDorsal, raceLength);
+        participants.add(participant);
+    }
+    public void ready()
+    {
+        //Create the n participants and add them to the list
+        //of participants.
+        for(int i = 0; i < nParticipants; i++)
+        {
+            registerParticipant(i);
+        }
+
+        System.out.println("Referee: Ready!");
+    }
+
+    public void set()
+    {
+        //Create the wrapping threads for each participant
+        //and add them to the list of participants threads.
+        for (Participant participant : participants)
+        {
+            participantThreads.add(new Thread(participant));
+        }
+        System.out.println("Referee: Set!");
+    }
+
+    public void go()
+    {
+        //Start the participants
+        for (Thread participantThread : participantThreads)
+        {
+            participantThread.start();
+        }
+        System.out.println("Referee: Go!");
+    }
+
+    public synchronized void finished(int _participantDorsal)
+    {
+        //TODO Understand what this does.
+        finishLineTape.countDown();
+        winner = String.valueOf(_participantDorsal);
+    }
+
+    public void run()
+    {
+        this.ready();
+        this.set();
+        this.go();
+
+        try
+        {
+            finishLineTape.await();
+        }
+        catch(InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        final int PARTICIPANTS = 2;
+        final int LENGTH_OF_RACE = 10500;
+
+        Referee ref = new Referee(LENGTH_OF_RACE);
+        ref.setNumberOfParticipants(PARTICIPANTS);
+
+        try
+        {
+            ref.start();
+            ref.join();
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Race interrupted");
+        }
+        System.out.printf("Referee: The winner is Participant %s\n\n", ref.winner);
+        System.out.println("Race ended\n");
+    }
 }
